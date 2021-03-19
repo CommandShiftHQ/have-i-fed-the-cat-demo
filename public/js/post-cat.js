b9bdd37201e113e70e8817c0d75152760d703462
ian-antking <ian.antking@gmail.com>
2021-03-19 19:01:41 +0000

    create cat posts multipart form data

---
+++ public/js/post-cat.js
@@ -0,0 +1,25 @@
+async function postCat(event) {
+  event.preventDefault();
+
+  const name = document.getElementById('catNameField').value;
+  const breed = document.getElementById('catBreedField').value;
+  const markings = document.getElementById('catMarkingsField').value;
+  const image = document.getElementById('catImageField').files[0];
+
+  const formData = new FormData();
+
+  formData.append('name', name);
+  formData.append('breed', breed);
+  formData.append('markings', markings);
+  formData.append('image', image);
+
+  const response = await fetch(`http://${window.location.host}/cats`, {
+      method: 'POST',
+      body: formData
+  })
+
+  const responseBody = await response.json()
+
+  console.log(responseBody);
+  window.location.replace(`http://${window.location.host}/`);
+}
