d5c9c3d27d4df13f7f1b722efe39e899da46ba86
ian-antking <ian.antking@gmail.com>
2021-07-15 15:35:17 +0100

    add field validation to cat model

--- src/models/cats.js
+++ src/models/cats.js
@@ -1,11 +1,34 @@
 module.exports = (sequelize, DataTypes) => {
   const schema = {
-    name: DataTypes.STRING,
-    breed: DataTypes.STRING,
-    markings: DataTypes.STRING,
-    lastFed: DataTypes.DATE
-  }
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: {
+          msg: 'name cannot be empty',
+        },
+      },
+    },
+    breed: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: {
+          msg: 'breed cannot be empty',
+        },
+      },
+    },
+    markings: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: {
+          msg: 'markings cannot be empty',
+        },
+      },
+    },
+    lastFed: DataTypes.DATE,
+  };
 
-  return sequelize.define('Cat', schema)
-}
-
+  return sequelize.define('Cat', schema);
+};
