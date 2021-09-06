a4e2e7cdfc8dd2675c54cd23690e157d679ed9ba
Ian King <ian.antking@gmail.com>
2021-09-06 12:55:06 +0100

    add mock stub and spy examples

--- tests/integration/create.test.js
+++ tests/integration/create.test.js
@@ -3,22 +3,50 @@ const { create } = require('../../src/controllers/cats')
 const catRepository = require('../../src/repository/cats');
 
 describe('create', () => {
+  let req, res
+
+  beforeEach(() => {
+    req = {
+      body: {
+        name: "Boris",
+        breed: "domestic shorthair",
+        markings: "tuxedo"
+      }
+    }
+
+    res = { status: () => { json: () => {} } }
+  })
+
   afterEach(() => {
     sinon.restore()
   })
 
-  // spy
-  it('it uses the catRepository to create a new cat', () => {
-    const req = {
-      body: {}
-    }
-    const res = {
-      status: () => { json: () => {} }
-    }
+  it('spy', () => {
     const createCatSpy = sinon.spy(catRepository, "createCat")
 
     create(req, res)
 
     sinon.assert.calledOnce(createCatSpy)
   })
+
+    it('spy', () => {
+      const createCatStub = sinon.stub(catRepository, "createCat").callsFake(data => Promise.resolve(data))
+  
+      create(req, res)
+  
+      sinon.assert.calledWith(createCatStub, req.body)
+    })
+
+  
+    it("mock", () => {
+      const mockCatRepository = sinon.mock(catRepository)
+      mockCatRepository.expects("createCat")
+        .once()
+        .withArgs(req.body)
+        .returns(Promise.resolve(req.body))
+  
+      create(req,res)
+  
+      mockCatRepository.verify()
+    })
 })
